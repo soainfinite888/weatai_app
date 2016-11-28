cf85071922348d1485397fba3b9bad468c20a873
chiachienhung <lisa5432126@gmail.com>
2016-11-28 15:03:59 +0800

    Add files via upload

--- controllers/weather.rb
+++ controllers/weather.rb
@@ -5,15 +5,16 @@ class WeataiApp < Sinatra::Base
 
   #get only one station weather data(from database) on homepage
   get "/?" do
-    result = FindWeather.call(params[:station])
+    #search_station = Station.call(params)
+    result = FindWeather.call(params)
 
     if result.success?
       @data = result.value
     else
-      flash[:error] = result.value.message #use flash, update by views
+    #  flash[:error] = result.value.message #use flash, update by views
     end
-    
-    slim:instant_weather.slim
+
+    slim:instant_weather
   end
 
  
