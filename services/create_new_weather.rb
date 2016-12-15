0437e62ac1d4ad6a9ea7749aea4450553b50acea
Neil <wer321011@gmail.com>
2016-12-15 10:32:00 -0500

    Modify

--- services/create_new_weather.rb
+++ services/create_new_weather.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# Loads data from API
 class CreateNewWeather
   extend Dry::Monads::Either::Mixin
   extend Dry::Container::Mixin
@@ -9,53 +8,15 @@ class CreateNewWeather
     icons = [params[:icon_weather], params[:icon_situation], params[:icon_side],
              params[:icon_activity], params[:icon_emotion], params[:icon_festival]]
     icons.each do |icon|
-      HTTP.post('http://localhost:9292/api/v0.1/user_weather',
-                json:{location: params[:location],
-                      icon: icon,
-                      upload_time: Time.now
-                })
+      result = HTTP.post('http://localhost:9292/api/v0.1/user_weather',
+                          json:{location: params[:location],
+                                icon: icon,
+                                upload_time: Time.now
+                        })
     end
+    Right(result.body)
+  rescue
+    Left(Error.new('Our servers failed - we are investigating!'))
   end
 end
-=begin
-    Dry.Transaction(container: self) do
-      step :validate_weather
-      step :call_api_to_load_weather
-      step :download_weather
-    end.call(params)
-  end
-
-  register :validate_weather, lambda { |params|
-    if params.success?
-      Right(url_request[:weather_url])
-    else
-      message = ErrorFlattener.new(
-        ValidationError.new(url_request)
-      ).to_s
-      Left(Error.new(message))
-    end
-  }
-
-  register :call_api_to_load_weather, lambda { |url|
-    begin
-      Right(HTTP.post("#{WeataiApp.config.Weatai_API}/weather",
-                      json: { url: url }))
-    rescue
-      Left(Error.new('Our servers failed - we are investigating!'))
-    end
-  }
-
-  register :download_weather, lambda { |http_result|
-    data = http_result.body.to_s
-    if http_result.status == 200
-      Right(AllWeatherRepresenter.new(weather).from_json(data))
-    else
-      message = ErrorFlattener.new(
-        ApiErrorRepresenter.new(ApiError.new).from_json(data)
-      ).to_s
-      Left(Error.new(message))
-    end
-  }
-end
-=end
 
