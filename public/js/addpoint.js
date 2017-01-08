44daa85431cc0f6fcdfcf7878eef69262fe31856
youyotsu <youyotsu@gmail.com>
2017-01-08 21:14:03 +0800

    colorful

--- public/js/addpoint.js
+++ public/js/addpoint.js
@@ -54,11 +54,12 @@ function initAutocomplete() {
     
     places.forEach(function(place) {
       var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
+        //url: place.icon,
+        url: '../img/point.png',
+        size: new google.maps.Size(50, 50),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
+        scaledSize: new google.maps.Size(50, 50)
       };
 
       // Create a marker for each place.
